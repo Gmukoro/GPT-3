2de21dd7830d205c195327bcc9d28bea0c0d7ab5
gmukoro <gmukoro3@gmail.com>
2023-10-29 00:11:38 +0100

    Components created and updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1>GPT-3</h1>
+      <h1>App</h1>
     </div>
   );
 };
---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./brand.css";
+
+function Brand() {
+  retu;
+  import "./brand.css";rn (
+    <div>Brand</div>
+  )
+}
+
+export default;
+import "./brand.css"; Brand
