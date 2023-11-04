10487824deabdb534c386ff0b288b228fa00be3c
gmukoro <gmukoro3@gmail.com>
2023-11-04 16:37:36 +0100

    Brands updated

--- src/containers/WhatGPT3/WhatGPT3.jsx
+++ src/containers/WhatGPT3/WhatGPT3.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./whatGPT3.css";
+import { Feature } from "../../components";
 
 function WhatGPT3() {
-  return <div>WhatGPT3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin " id="whgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
+  );
 }
 
 export default WhatGPT3;
